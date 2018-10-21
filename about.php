d49cbda4ba2f83ae15856c4ebf333cde27426ea3
pawanrai9999 <pawanrai9999@gmail.com>
2018-10-22 02:48:36 +0530

    Added to current

--- about.php
+++ about.php
@@ -37,10 +37,10 @@
                 <div class="collapse navbar-collapse" id="navbarTogglerA1">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
-                            <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+                            <a class="nav-link" href="index.php">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="about.php">About US</a>
+                            <a class="nav-link" href="about.php">About US<span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="contact.php">Contact US</a>
