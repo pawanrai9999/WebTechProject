0256ca45013b177c21ecba27ae56e514f3d483bb
cipheres <cipheres@gmail.com>
2018-10-21 17:28:10 +0530

    material design setup

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<?php
+
+/* 
+ * Author: Pawan
+ * Date: Oct 21, 2018
+ * File Name: index.php
+ * Lisence: MIT
+ */
+?>
+<!doctype html>
+<html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="utf-8" />
+        <title>Geek</title>
+        <link rel="stylesheet" href="assets/css/matui.min.css">
+        <script type="text/javascript" src="assets/js/material.js"></script>
     </head>
+    
     <body>
-        <?php
-        // put your code here
-        ?>
+       <!-- Accent-colored raised button with ripple -->
+<!-- Accent-colored raised button with ripple -->
+<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+  Button
+</button>
+
+
     </body>
 </html>
